cdf34213e526f827e06362ffa1481d14c81fe995
xiaoming <lininruc@gmail.com>
2024-04-29 21:56:46 +0800

    add mvs

--- batch_run.py
+++
@@ -1,9 +0,0 @@
-# English -> English subtitle
-import os
-print("Your Youtube Address:")
-youtbAddr = input()
-## https://www.youtube.com/watch?v=MnrJzXM7a6o
-os.system("yt-dlp -o 'ASurvey.%(ext)s' " + youtbAddr)
-##os.system("whisper ASurvey.webm --task translate --language Chinese --model small --fp16 False")
-os.system("whisper ASurvey.webm --task transcribe --language English --model small --fp16 False")
-os.system("python3 app.py")
