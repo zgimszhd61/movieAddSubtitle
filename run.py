0a42d9f1948ca846e30c99a62b170b80945eb8a7
zgimszhd61 <114722053+zgimszhd61@users.noreply.github.com>
2024-04-29 17:00:32 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("yt-dlp -o "ASurvey.%(ext)s" https://www.youtube.com/watch?v=MnrJzXM7a6o")
+os.system("whisper ASurvey.webm --task translate --language Chinese --model small")
+os.system("python3 app.py")
