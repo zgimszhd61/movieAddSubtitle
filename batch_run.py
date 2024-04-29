3d6b7a61958b80e2750fb68eba7e3e1c199fdf55
zgimszhd61 <114722053+zgimszhd61@users.noreply.github.com>
2024-04-29 17:40:45 +0800

    Update batch_run.py

--- batch_run.py
+++ batch_run.py
@@ -3,5 +3,6 @@ print("your Youtube Address:")
 youtbAddr = input()
 ## https://www.youtube.com/watch?v=MnrJzXM7a6o
 os.system("yt-dlp -o 'ASurvey.%(ext)s' " + youtbAddr)
-os.system("whisper ASurvey.webm --task translate --language Chinese --model small --fp16 False")
+##os.system("whisper ASurvey.webm --task translate --language Chinese --model small --fp16 False")
+os.system("whisper ASurvey.webm --task transcribe --language English --model small --fp16 False")
 os.system("python3 app.py")
